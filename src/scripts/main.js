0779a16fdc07afa133f6c677ac9e91f3ee2c17c7
chlodav <153681219+chlodav@users.noreply.github.com>
2026-03-15 14:08:56 -0700

    fixed mistaked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function handleMenuScrollLock() {
+  if (window.location.hash === '#menu') {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+}
+
+window.addEventListener('hashchange', handleMenuScrollLock);
+handleMenuScrollLock();
